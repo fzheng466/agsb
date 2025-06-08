939674fd5333df5662b3622b78a94bc1419ee8f7
fzheng466 <fzheng488@gmail.com>
2025-06-09 04:10:52 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -32,8 +32,8 @@ CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的�
 
 # ====== 全局可配置参数（可直接在此处修改） ======
 USER_NAME = "fengzheng"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
+UUID = "302e5a5a-f84f-45b0-9d1d-a8b0cf6dd397"                     # UUID，留空则自动生成
+PORT = 4999                   # Vmess端口，留空或0则自动生成
 DOMAIN = " fengzheng.dpdns.org"                   # 域名，留空则自动获取
 CF_TOKEN = "eyJhIjoiMmYzYTU3ZjYzMDM0ZGJkY2NjMjcwZGJkZDA2YzkxYjEiLCJ0IjoiMDQ4ZGYxMjEtYTU0NC00MWFlLWE4OWItYWI1NWEwZDk5NTJiIiwicyI6Ik1tTTBZamRqTldVdE56azVNeTAwT1RBMExUaG1PRFV0TTJZNE9EVmxZV1l4WVRZdyJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
